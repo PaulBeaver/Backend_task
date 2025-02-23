786e009a74a561c434ad8550bc5bd744c1ac6295
Pavel Bobrov <pavel.bobrou@innowise.com>
2025-02-23 16:14:16 +0100

    issue fix 2

--- tests/fixtures/start_db.py
+++ tests/fixtures/start_db.py
@@ -8,34 +8,36 @@ from ..functions import wait_for_postgres
 
 @pytest.fixture(scope="session", autouse=True)
 def start_postgres_container():
-    try:
-        subprocess.run(
-            [
-                "docker",
-                "compose",
-                "-f",
-                "docker-compose-tests.yml",
-                "up",
-                "-d",
-            ],
-            check=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-        wait_for_postgres()
-
-        env = dict(os.environ)
-        env["PYTEST"] = "1"
-
-        subprocess.run(["alembic", "upgrade", "head"], env=env)
-        yield
-    finally:
+    if os.getenv("CI") or os.getenv("GITHUB_ACTIONS"):
+        print("CI environment detected – skipping docker compose up for tests.")
+    else:
         try:
             subprocess.run(
-                ["docker", "compose", "-f", "docker-compose-tests.yml", "down"],
+                [
+                    "docker",
+                    "compose",
+                    "-f",
+                    "docker-compose-tests.yml",
+                    "up",
+                    "-d",
+                ],
                 check=True,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
             )
-        except subprocess.CalledProcessError as e:
-            pass
+            wait_for_postgres()
+            env = dict(os.environ)
+            env["PYTEST"] = "1"
+            subprocess.run(["alembic", "upgrade", "head"], env=env)
+            yield
+        finally:
+            try:
+                subprocess.run(
+                    ["docker", "compose", "-f", "docker-compose-tests.yml", "down"],
+                    check=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                )
+            except subprocess.CalledProcessError:
+                pass
+    yield
